fb1fef812d2bd276e7f80c6ca51ffb03a5be9a0c
Remy <107559570+hrszpuk@users.noreply.github.com>
2022-09-21 23:17:44 +0100

    Added colours to CLI

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 mod bank;
 
 use std::io::Write;
+use colored::Colorize;
 
 fn main() {
-    println!("ATM interface");
+    println!("{}", "ATM interface".green().bold());
 
     // We use this for storing all inputs from the user throughout the program
     let mut buffer = String::new();
@@ -23,7 +24,7 @@ fn main() {
         match buffer.trim() {
 
             // If the user wants to view their balance.
-            "balance" => println!("Your current balance is: ${}.", account.get_balance()),
+            "balance" => println!("Your current balance is: ${}.", account.get_balance().to_string().green()),
 
             // For transferring money from the user's account to another
             "transfer" => transfer(&mut buffer, &mut account, &mut other_accounts),
@@ -33,10 +34,10 @@ fn main() {
                 for (date, name, amount) in &account.payments {
                     if *name == *account.get_name() {
                         println!("Timestamp: {}", date);
-                        println!("           └ Withdrew ${}", amount);
+                        println!("           └ Withdrew ${}", amount.to_string().red());
                     } else {
                         println!("Timestamp: {}", date);
-                        println!("           └ Transferred ${} to \"{}\"", amount, name);
+                        println!("           └ Transferred ${} to \"{}\"", amount.to_string().red(), name.blue());
                     }
                 }
             },
@@ -52,33 +53,36 @@ fn main() {
             // Shows name and balance
             "info" => {
                 println!(
-                    "Account information\nName: {}\nBalance: ${}",
-                    account.get_name(),
-                    account.get_balance(),
+                    "Account information\n{} {}\n{} ${}",
+                    "Name:".bold(),
+                    account.get_name().blue(),
+                    "Balance:".bold(),
+                    account.get_balance().to_string().green(),
                 )
             }
 
             //  Show help message
             "help" => {
                 println!("Available commands:");
-                println!("\thelp: show this message");
-                println!("\tbalance: shows your current balance");
-                println!("\ttransfer: transfer funds to another individual");
-                println!("\twithdraw: withdraw funds from your account");
-                println!("\tpayments: view payment history");
-                println!("\tinfo: view account information");
-                println!("\texit: quits the program (quit also works)");
+                println!("\t{}: show this message", "help".underline().purple());
+                println!("\t{}: shows your current balance", "balance".underline().purple());
+                println!("\t{}: transfer funds to another individual", "transfer".underline().purple());
+                println!("\t{}: withdraw funds from your account", "withdraw".underline().purple());
+                println!("\t{}: view payment history", "payments".underline().purple());
+                println!("\t{}: view account information", "info".underline().purple());
+                println!("\t{}: quits the program (quit also works)", "exit".underline().purple());
             },
 
             // If we ever get an unexpected input we report the issue to the user
             _ => println!(
-                "error: \"{}\" is not an available command. \nUse \"help\" to view available commands.",
-                buffer.trim()
+                "{}: {} is not an available command. \nUse \"help\" to view available commands.",
+                "Error".red(),
+                buffer.trim().underline().bright_red()
             ),
         }
         buffer.clear()
     }
-    println!("Closed ATM interface.");
+    println!("{}", "Closed ATM interface.".red().bold());
 }
 
 /// For transferring money from the user's bank account to another bank account
@@ -87,7 +91,7 @@ fn transfer(mut buffer: &mut String, account: &mut bank::Bank, other_accounts: &
     // List all the recipients the user can send to...
     println!("Please choose the recipient you wish to send money to.");
     for (index, account) in other_accounts.iter().enumerate() {
-        println!("[{}] {}", index+1, account.get_name());
+        println!("[{}] {}", index+1, account.get_name().blue());
     }
 
     // Get which recipient the user wants to send money to...
@@ -103,7 +107,7 @@ fn transfer(mut buffer: &mut String, account: &mut bank::Bank, other_accounts: &
             return;
         },
         Err(_) => {
-            println!("\"{}\" is not a valid recipient number!", buffer.trim());
+            println!("{} is not a valid recipient number!", buffer.trim().red().underline());
             return;
         }
     };
@@ -111,14 +115,14 @@ fn transfer(mut buffer: &mut String, account: &mut bank::Bank, other_accounts: &
 
     // Find the amount of money the user wants to send...
     output_and_read_to_buffer(
-        format!("How much would you like to transfer to {}? $", recipient.get_name())
+        format!("How much would you like to transfer to {}? $", recipient.get_name().blue())
             .as_str(),
         &mut buffer,
     );
     let amount = match buffer.trim().parse() {
         Ok(value) => value,
         Err(_) => {
-            println!("\"{}\" is not a valid transfer amount!", buffer.trim());
+            println!("{} is not a valid transfer amount!", buffer.trim().red().underline());
             return;
         }
     };
@@ -127,11 +131,11 @@ fn transfer(mut buffer: &mut String, account: &mut bank::Bank, other_accounts: &
     match account.transfer(recipient, amount) {
         Ok(bal) => println!(
             "Transferred ${} to {}!\nRemaining balance: ${}",
-            amount,
-            recipient.get_name(),
-            bal,
+            amount.to_string().green(),
+            recipient.get_name().blue(),
+            bal.to_string().green(),
         ),
-        Err(_) => println!("An issue occurred with your transfer!"),
+        Err(_) => println!("{}", "An issue occurred with your transfer!".red()),
     };
 }
 
@@ -146,7 +150,7 @@ fn withdraw(mut buffer: &mut String, account: &mut bank::Bank) {
     let amount = match buffer.trim().parse::<f32>() {
         Ok(value) => value,
         Err(_) => {
-            println!("\"{}\" is not a valid withdraw amount!", buffer.trim());
+            println!("{} is not a valid withdraw amount!", buffer.trim().red().underline());
             return;
         }
     };
@@ -154,9 +158,9 @@ fn withdraw(mut buffer: &mut String, account: &mut bank::Bank) {
     // Withdrawing the amount from the user's balance
     match account.withdraw(amount) {
         Ok(bal) => {
-            println!("You withdrew ${}!\nRemaining balance: ${}", amount, bal);
+            println!("You withdrew ${}!\nRemaining balance: ${}", amount.to_string().blue(), bal.to_string().green());
         },
-        Err(_) => println!("You cannot withdraw ${} as it exceeds your balance!", amount),
+        Err(_) => println!("You cannot withdraw ${} as it exceeds your balance!", amount.to_string().red()),
     };
 }
 
@@ -165,7 +169,7 @@ fn register() -> bank::Bank {
     let mut balance_buffer = String::new();
     let balance: f32;
 
-    println!("Register a name and starting balance to continue...");
+    println!("Register a {} and {} to continue...", "name".underline(), "starting balance".underline());
 
     output_and_read_to_buffer(
         "Name: ",
@@ -180,7 +184,8 @@ fn register() -> bank::Bank {
     balance = match balance_buffer.trim().parse::<f32>() {
         Ok(bal) => bal,
         Err(_) => {
-            println!("\"{}\" is not a valid starting balance!", balance_buffer.trim());
+            println!("{}",
+                format!("{} is not a valid starting balance!", balance_buffer.trim()).red());
             println!("Re-starting registry process.");
             return register();
         }
@@ -194,7 +199,7 @@ fn register() -> bank::Bank {
 /// Prints the output, and then reads a line and slaps it into the buffer
 fn output_and_read_to_buffer(output: &str, buffer: &mut String) {
     buffer.clear();
-    print!("{}", output);
+    print!("{}", output.bold());
     std::io::stdout()
         .flush()
         .expect("Failed to flush output stream!");
